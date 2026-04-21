18538b7a90bfd17586c38ca9457b2bc20f1a3cb1
Swayam Atal <atalswayam28@gmail.com>
2026-04-22 02:49:38 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
