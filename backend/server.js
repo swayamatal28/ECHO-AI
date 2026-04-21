8680d37dc95070b2d12f7d9b5296e50fab48eef6
Swayam Atal <atalswayam28@gmail.com>
2026-04-22 02:15:32 +0530

    resolving cors issue6

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ const app = express();
 connectDB();
 app.use(cors({
   origin: [
-    "echo-ai-git-main-swayams-projects-123d6f79.vercel.app", // Removed the "/" at the end
-    "echo-8emlxfud0-swayams-projects-123d6f79.vercel.app" // Add your main project domain here too
+    "https://echo-ai-git-main-swayams-projects-123d6f79.vercel.app", // Removed the "/" at the end
+    "https://echo-8emlxfud0-swayams-projects-123d6f79.vercel.app" // Add your main project domain here too
   ], 
   credentials: true
 }));
